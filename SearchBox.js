0be1e27cfee380140c49651988b541906042c498
Avishkar Wadbudhe <avwadbudhe78@gmail.com>
2023-01-27 23:46:55 +0530

    updated search box

--- SearchBox.js
+++ SearchBox.js
@@ -1,42 +1,42 @@
-const searchbox = document.querySelector(".search"),
-SelectButton = searchbox.querySelector(".select-btn"),
-searchInp = searchbox.querySelector("input"),
-options = searchbox.querySelector(".options");
+const wrapper = document.querySelector(".wrapper"),
+selectBtn = wrapper.querySelector(".select-btn"),
+searchInp = wrapper.querySelector("input"),
+options = wrapper.querySelector(".options");
 
 let countries = ["Afghanistan", "Algeria", "Argentina", "Australia", "Bangladesh", "Belgium", "Bhutan",
-             "Brazil", "Canada", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
-             "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
-             "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
-             "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
-             "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+                 "Brazil", "Canada", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
+                 "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
+                 "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
+                 "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
+                 "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
 
 function addCountry(selectedCountry) {
-options.innerHTML = "";
-countries.forEach(country => {
-    let isSelected = country == selectedCountry ? "selected" : "";
-    let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
-    options.insertAdjacentHTML("beforeend", li);
-});
+    options.innerHTML = "";
+    countries.forEach(country => {
+        let isSelected = country == selectedCountry ? "selected" : "";
+        let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
+        options.insertAdjacentHTML("beforeend", li);
+    });
 }
 addCountry();
 
 function updateName(selectedLi) {
-searchInp.value = "";
-addCountry(selectedLi.innerText);
-searchbox.classList.remove("active");
-SelectButton.firstElementChild.innerText = selectedLi.innerText;
+    searchInp.value = "";
+    addCountry(selectedLi.innerText);
+    wrapper.classList.remove("active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
 }
 
 searchInp.addEventListener("keyup", () => {
-let arr = [];
-let searchWord = searchInp.value.toLowerCase();
-arr = countries.filter(data => {
-    return data.toLowerCase().startsWith(searchWord);
-}).map(data => {
-    let isSelected = data == SelectButton.firstElementChild.innerText ? "selected" : "";
-    return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
-}).join("");
-options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
+    let arr = [];
+    let searchWord = searchInp.value.toLowerCase();
+    arr = countries.filter(data => {
+        return data.toLowerCase().startsWith(searchWord);
+    }).map(data => {
+        let isSelected = data == selectBtn.firstElementChild.innerText ? "selected" : "";
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
 });
 
-SelectButton.addEventListener("click", () => searchbox.classList.toggle("active"));
+selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
